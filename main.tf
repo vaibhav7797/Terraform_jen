c0b4b0ea1bb460c5ea4ba47431293bf348971738
vaibhav pendhare <vaibhav.pendhare@clouddrove.com>
2022-08-25 11:54:43 +0530

    jenkins updated

--- main.tf
+++ main.tf
@@ -2,6 +2,13 @@ provider "aws" {
   region = "us-east-1"
 }
 
+terraform {
+  backend "s3" {
+    bucket = "my-dev-tf-state-bucket07797"
+    key    = "main"
+    region = "us-east-1"
+  }
+} 
 
 module "vpc" {
   source  = "clouddrove/vpc/aws"
