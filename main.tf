afb2027a2a3330bf5da60ccdc82d3a210098357d
vaibhav pendhare <vaibhav.pendhare@clouddrove.com>
2022-08-25 14:28:20 +0530

    jenkins

--- main.tf
+++ main.tf
@@ -11,11 +11,11 @@ terraform {
 } 
 
 # module "vpc" {
-  source  = "clouddrove/vpc/aws"
-  version = "0.15.1"
+#   source  = "clouddrove/vpc/aws"
+#   version = "0.15.1"
 
-  name        = "moneyceo"
-  environment = "test"
-  label_order = ["name","environment"]
-  cidr_block  = "10.0.0.0/16"
-}
+#   name        = "moneyceo"
+#   environment = "test"
+#   label_order = ["name","environment"]
+#   cidr_block  = "10.0.0.0/16"
+# }
